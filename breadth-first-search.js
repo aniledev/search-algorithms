45393cb66c69937701a506a7532421ce5576e087
aniledev <elinamcgill@gmail.com>
2021-02-17 16:40:51 -0500

    FEAT: write a method to perform breadth first search of binary tree

--- breadth-first-search.js
+++ breadth-first-search.js
@@ -1,12 +1,30 @@
 class BinarySearchTree {
-    // use constructor method to set up a basic binary search tree that is empty/null
-    constructor(key = null, value = null, parent = null) {
-      this.key = key;
-      this.value = value;
-      this.parent = parent;
-      this.left = null;
-      this.right = null;
-    }
-  
-    
+  // use constructor method to set up a basic binary search tree that is empty/null
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
   }
+
+  bfs(tree, values = []) {
+    const queue = new Queue(); // Assuming a Queue is implemented (refer to previous lesson on Queue)
+    const node = tree.root;
+    queue.enqueue(node);
+    while (queue.length) {
+      const node = queue.dequeue(); //remove from the queue
+      values.push(node.value); // add that value from the queue to an array
+
+      if (node.left) {
+        queue.enqueue(node.left); //add left child to the queue
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right); // add right child to the queue
+      }
+    }
+
+    return values;
+  }
+}
