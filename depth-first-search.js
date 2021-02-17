f0736415d5ddbf1ba589d747c7345a78237e0dc3
aniledev <elinamcgill@gmail.com>
2021-02-17 16:16:43 -0500

    FEAT: write depth first search method

--- depth-first-search.js
+++ depth-first-search.js
@@ -1,9 +1,28 @@
+/*
+In a DFS you start from a given node (usually the root) and traverse as far as you can down. 
+When you reach a node, which has no children to visit or all nodes on its path have been visited, 
+you start backtracking. Let's take a look at how this works. T
+ */
+
 class BinarySearchTree {
-    // use constructor method to set up a basic binary search tree
-    constructor(key = null, value = null, parent = null) {
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
-        this.left = null;
-        this.right = null;
+  // use constructor method to set up a basic binary search tree that is empty/null
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  dfs(values = []) {
+    if (this.left) {
+      values = this.left.dfs(values);
     }
+    values.push(this.value);
+
+    if (this.right) {
+      values = this.right.dfs(values);
+    }
+    return values;
+  }
+}
