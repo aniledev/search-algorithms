d6163daf968c3bd887871a67a296ab715e9fce0e
aniledev <elinamcgill@gmail.com>
2021-02-17 16:19:55 -0500

    FEAT: construct a binary search tree for use in breadth first search

---
+++ breadth-first-search.js
@@ -0,0 +1,12 @@
+class BinarySearchTree {
+    // use constructor method to set up a basic binary search tree that is empty/null
+    constructor(key = null, value = null, parent = null) {
+      this.key = key;
+      this.value = value;
+      this.parent = parent;
+      this.left = null;
+      this.right = null;
+    }
+  
+    
+  }
